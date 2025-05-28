9e63542bdad399c26a74b6ace3d8f77e7f26fbfd
karimazizc <karimazizc@gmail.com>
2025-05-28 20:25:44 +0700

    updated new

---
+++ static/js/background.js
@@ -0,0 +1,44 @@
+
+ particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 60,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#999999"
+        },
+        "shape": {
+          "type": "circle"
+        },
+        "opacity": {
+          "value": 0.3
+        },
+        "size": {
+          "value": 3
+        },
+        "move": {
+          "enable": true,
+          "speed": 1
+        },
+        "line_linked": {
+          "enable": true
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false
+          },
+          "onclick": {
+            "enable": false
+          },
+          "resize": true
+        }
+      },
+      "retina_detect": true
+    });
